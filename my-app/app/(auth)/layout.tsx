c2deed9a0d13015d56ba50a8012eb24983c0f7b7
nana <qhgus4856@gmail.com>
2025-01-08 13:53:27 +0900

    푸터 - 로그인페이지에서 제외, 그룹라우팅 사용

---
+++ my-app/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/footer";
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main>{children}</main>
-        <Footer></Footer>
       </body>
     </html>
   );
