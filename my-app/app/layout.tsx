ad27ed7503a98d5260cf299a83e8c662feba3ec7
nana <qhgus4856@gmail.com>
2025-01-09 23:55:12 +0900

    meta 태그 추가, 네이버 서치어드바이저

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -15,8 +15,14 @@ const geistMono = localFont({
 
 // head 요소
 export const metadata: Metadata = {
-  title: "Batteseo",
-  description: "Developed by Next14 App Router",
+  title: "봉화밭에서",
+  description: "봉화에서 직접 생산한 들깨",
+  keywords: "봉화, 들깨, 들기름, 들깨가루",
+  verification: {
+    other: {
+      "naver-site-verification" : "1fce1f4d7e78df2357b056f4399500e74df4bc0f"
+    }
+  }
 };
 
 export default function RootLayout({
